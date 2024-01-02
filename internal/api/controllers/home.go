c45717f0f1fd5bcc483a194727fa14f94dcf2180
zak-perkins <isaac.perkins.1@gmail.com>
2024-01-02 22:46:29 +1100

    step 2

--- internal/api/controllers/home.go
+++ internal/api/controllers/home.go
@@ -9,10 +9,7 @@ type home struct {
 	copy  string
 }
 
-func GetHome() func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		//store.Migrate(d)
-		h := home{title: "velkum", copy: "asdf"}
-		Result(w, h)
-	}
+func GetHome(w http.ResponseWriter, r *http.Request) {
+	h := home{title: "velkum", copy: "asdf"}
+	Result(w, h)
 }
